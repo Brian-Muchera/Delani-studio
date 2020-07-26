494dfb9e20637099a27866b22bacfc11ab7c1cc3
Brian-Muchera <mucherabrian2@gmail.com>
2020-07-26 23:06:00 +0300

    modify scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,93 @@
 $(document).ready(function () {
     $(".design-img").click(function () {
-        $(".design-text").toggle();
-        $(".design-img").hide();
+      $(".design-text").toggle();
+      $(".design-img").hide();
     });
     $(".development-img").click(function () {
-        $(".development-text").toggle();
-        $(".development-img").hide();
+      $(".development-text").toggle();
+      $(".development-img").hide();
+    });
+    $(".product-img").click(function () {$(document).ready(function () {
+    $(".design-img").click(function () {
+      $(".design-text").toggle();
+      $(".design-img").hide();
+    });
+    $(".development-img").click(function () {
+      $(".development-text").toggle();
+      $(".development-img").hide();
     });
     $(".product-img").click(function () {
-        $(document).ready(function () {
-            $(".design-img").click(function () {
-                $(".design-text").toggle();
-                $(".design-img").hide();
-            });
-            $(".development-img").click(function () {
-                $(".development-text").toggle();
-                $(".development-img").hide();
-            });
-            $(".product-img").click(function () {
-                $(".product-text").toggle();
-                $(".product-img").hide();
-            });
-            $(".design-text").click(function () {
-                $(".design-img").toggle();
-                $(".design-text").hide();
-            });
-            $(".development-text").click(function () {
-                $(".development-img").toggle();
-                $(".development-text").hide();
-            });
-            $(".product-text").click(function () {
-                $(".product-img").toggle();
-                $(".product-text").hide();
-            });
-        });
-
-        $(document).ready(function () {
-            $(".col-md-3").mouseover(function () {
-                $(".col-md-3").css("border-size", "5px");
-            });
-        });
-
-
-        
-        function reset() {
-            document.getElementById("myform").reset();
-        }
-        $(".product-text").toggle();
-        $(".product-img").hide();
+      $(".product-text").toggle();
+      $(".product-img").hide();
     });
     $(".design-text").click(function () {
-        $(".design-img").toggle();
-        $(".design-text").hide();
+      $(".design-img").toggle();
+      $(".design-text").hide();
     });
     $(".development-text").click(function () {
-        $(".development-img").toggle();
-        $(".development-text").hide();
+      $(".development-img").toggle();
+      $(".development-text").hide();
     });
     $(".product-text").click(function () {
-        $(".product-img").toggle();
-        $(".product-text").hide();
+      $(".product-img").toggle();
+      $(".product-text").hide();
     });
-});
+  });
+  
+  $(document).ready(function () {
+    $(".col-md-3").mouseover(function () {
+      $(".col-md-3").css("border-size", "5px");
+    });
+  });
+  
+  
+  function myname() {
+    var name = document.getElementById("name").value;
+    if (name === "" || email == "") {
+      alert("Please enter name and email correctly...");
+    } else {
+      alert(name + "" + "we have recieved your message");
+    };
+  };
+  
+  
+  function reset() {
+    document.getElementById("myform").reset();
+  }
+      $(".product-text").toggle();
+      $(".product-img").hide();
+    });
+    $(".design-text").click(function () {
+      $(".design-img").toggle();
+      $(".design-text").hide();
+    });
+    $(".development-text").click(function () {
+      $(".development-img").toggle();
+      $(".development-text").hide();
+    });
+    $(".product-text").click(function () {
+      $(".product-img").toggle();
+      $(".product-text").hide();
+    });
+  });
+  
+  $(document).ready(function () {
+    $(".col-md-3").mouseover(function () {
+      $(".col-md-3").css("border-size", "5px");
+    });
+  });
+  
+  
+  function myname() {
+    var name = document.getElementById("name").value;
+    if (name === "" || email == "") {
+      alert("Please enter name and email correctly...");
+    } else {
+      alert(name + "" + "we have recieved your message");
+    };
+  };
+  
+  
+  function reset() {
+    document.getElementById("myform").reset();
+  }
