c1eb239028e9665eabdfaa2a803541b757ca9151
Brian-Muchera <mucherabrian2@gmail.com>
2020-07-26 21:22:31 +0300

    added submit button

--- js/scripts.js
+++ js/scripts.js
@@ -39,4 +39,25 @@ $(document).ready(function () {
               $(".col-md-3").mouseover(function () {
                 $(".col-md-3").css("border-size", "5px");
               });
-            });   
+            }); 
+            
+            function reset() {
+                document.getElementById("myform").reset();
+              }
+                  $(".product-text").toggle();
+                  $(".product-img").hide();
+                });
+                $(".design-text").click(function () {
+                  $(".design-img").toggle();
+                  $(".design-text").hide();
+                });
+                $(".development-text").click(function () {
+                  $(".development-img").toggle();
+                  $(".development-text").hide();
+                });
+                $(".product-text").click(function () {
+                  $(".product-img").toggle();
+                  $(".product-text").hide();
+                });
+              });
+              
