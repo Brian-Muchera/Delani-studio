3bb1ea0e92c4303a54fcf23ccee97a69b30df6aa
Brian-Muchera <mucherabrian2@gmail.com>
2020-07-26 19:49:16 +0300

    modify scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
     $(".design-img").click(function () {
-      $(".design-text").toggle();
-      $(".design-img").hide();
+        $(".design-text").toggle();
+        $(".design-img").hide();
     });
     $(".development-img").click(function () {
-      $(".development-text").toggle();
-      $(".development-img").hide();
+        $(".development-text").toggle();
+        $(".development-img").hide();
     });
-    $(".product-img").click(function () {$(document).ready(function () {
-        $(".design-img").click(function () {
-          $(".design-text").toggle();
-          $(".design-img").hide();
-        });
+    $(".product-img").click(function () {
+        $(document).ready(function () {
+            $(".design-img").click(function () {
+                $(".design-text").toggle();
+                $(".design-img").hide();
+            });
+            $(".development-img").click(function () {
+                $(".development-text").toggle();
+                $(".development-img").hide();
+            });
+            $(".product-img").click(function () {
+                $(".product-text").toggle();
+                $(".product-img").hide();
+            });   
